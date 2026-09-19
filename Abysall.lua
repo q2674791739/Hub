e9fb398dd4ea75c535d97813d693774b45510779
q2674791739 <2037991739@qq.com>
2026-09-19 13:39:03 +0800

    Update Abysall.lua

--- Abysall.lua
+++ Abysall.lua
@@ -4114,7 +4114,9 @@ Functions.TriggerPrompt = function(Prompt)
 	if Prompt:GetAttribute("AutoInteractIgnore") then return end
 	if Prompt.Name == "PushPrompt" and Options.AutoInteractIgnoreList.Value["矿车"] then return end
 	if (Prompt.Parent.Name == "GoldPile" or Prompt.Parent.Name == "StardustPickup") and Options.AutoInteractIgnoreList.Value["货币"] then return end
-
+    if (Prompt.Parent.Name == "SeatPart" or Prompt.Parent.Name == "Collider" or Prompt.Name == "SeatPrompt") and Options.AutoInteractIgnoreList.Value["椅子"] then return end
+    if (Prompt.Parent.Name == "Flap" or Prompt.Name == "TrashcanPrompt") and Options.AutoInteractIgnoreList.Value["垃圾桶"] then return end
+    if (Prompt.Parent.Name == "Vendor_ShakelightVendingMachine" or Prompt.Name == "VendorPrompt") and Options.AutoInteractIgnoreList.Value["售货机"] then return end
 	if Prompt.Parent.Name == "Bandage" then
 		local BPack = Functions.HasItem("BandagePack")
 		if Humanoid.Health >= Humanoid.MaxHealth and not BPack then return end
