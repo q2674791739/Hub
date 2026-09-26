41165bcf922a958494fdbb83abc06c93dd22563c
q2674791739 <2037991739@qq.com>
2026-09-26 08:30:50 +0800

    123

--- Abysall.lua
+++ Abysall.lua
@@ -1190,8 +1190,8 @@ Toggles.AutoInteractToggle:AddKeyPicker("AutoInteractKeybind", {
 })
 Groupboxes.Self_Automation:AddDropdown("AutoInteractIgnoreList", {
 	Text = "忽略列表",
-	Values = { "故障碎片", "Jeff 物品", "掉落物品", "矿车", "椅子", "垃圾桶", "售货机", "档案终端", "打印机", "锁", "货币" },
-Default = { "故障碎片", "Jeff 物品", "掉落物品", "矿车", "椅子", "垃圾桶", "售货机", "档案终端", "打印机"},
+	Values = { "故障碎片", "Jeff 物品", "掉落物品", "矿车", "锁", "货币" },
+Default = { "故障碎片", "Jeff 物品", "掉落物品", "矿车", },
 	Multi = true, AllowNull = true
 })
 Groupboxes.Self_Automation:AddDivider()
